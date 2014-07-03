17a2b559b8073376c33023051d4afdebbd16a8d8
Mikdiet <mikdiet@gmail.com>
2014-07-03 18:15:56 +0300

    add logging

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<?php 
+require('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("Hello World");
+
+phpinfo(); 
+?>
